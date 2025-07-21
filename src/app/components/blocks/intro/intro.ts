c6056ad8c19f71bdd7f2dcca481a95d78067c4b2
ZakhaRDevel <zakhar.devel@gmail.com>
2025-07-21 03:48:51 +0300

    feat(app): add intro anim

--- src/app/components/blocks/intro/intro.ts
+++ src/app/components/blocks/intro/intro.ts
@@ -1,9 +1,20 @@
-import {Component, inject} from '@angular/core';
+import {
+  Component,
+  inject,
+  AfterViewInit,
+  ElementRef,
+  viewChildren,
+  viewChild,
+} from '@angular/core';
 import {Container} from '../container/container';
 import {NgOptimizedImage} from '@angular/common';
 import {Button} from '../../ui/button/button';
 import {ModalService} from '../../../services/modal';
 import {SimModal} from '../../modal/sim-modal/sim-modal';
+import {gsap} from 'gsap';
+import {CSSPlugin} from 'gsap/CSSPlugin';
+
+gsap.registerPlugin(CSSPlugin);
 
 @Component({
   selector: 'app-intro',
@@ -11,8 +22,55 @@ import {SimModal} from '../../modal/sim-modal/sim-modal';
   templateUrl: './intro.html',
   styleUrl: './intro.scss',
 })
-export class Intro {
+export class Intro implements AfterViewInit {
+  images = viewChildren<ElementRef>('img');
+  texts = viewChildren<ElementRef>('text');
+  btn = viewChild<ElementRef>('btn');
   modal = inject(ModalService);
+  private elementRef = inject(ElementRef);
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.initAnimation();
+    }, 100);
+  }
+
+  private initAnimation() {
+    const tl = gsap.timeline({defaults: {ease: 'power3.out'}});
+
+    const textElements = this.texts();
+    const animImgs = this.images();
+    const btn = this.btn()?.nativeElement;
+
+    if (textElements.length === 0 || animImgs.length === 0) {
+      return;
+    }
+
+    const [one, two, three] = textElements.map((ref) => ref.nativeElement);
+    const imgElements = animImgs.map((ref) => ref.nativeElement);
+
+    tl.set(one, {y: -10, clipPath: 'inset(0 0 100% 0)'})
+      .set(two, {y: -10, clipPath: 'inset(0 0 100% 0)'})
+      .set(three, {y: -10, clipPath: 'inset(0 0 100% 0)'})
+      .set(imgElements, {scale: 0, opacity: 0, rotation: -180})
+      .set(btn, {opacity: 0})
+      .to(one, {y: 0, clipPath: 'inset(0 0 0% 0)', duration: 1.5})
+      .to(two, {y: 0, clipPath: 'inset(0 0 0% 0)', duration: 1.5}, '-=1.0')
+      .to(
+        imgElements,
+        {
+          scale: 1,
+          opacity: 1,
+          rotation: 0,
+          duration: 0.8,
+          stagger: 0.2,
+          ease: 'back.out(1.7)',
+        },
+        '-=1.5'
+      )
+      .to(three, {y: 0, clipPath: 'inset(0 0 0% 0)', duration: 1.5}, '-=1.0')
+      .to(btn, {opacity: 1, duration: 0.8}, '-=1.0');
+  }
 
   onOpenModal() {
     this.modal.open(SimModal, {data: {title: 'Выбери тип сим-карты'}});
